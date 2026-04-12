d6422c1b9575ab34d39fc861e6a9c00622f4298c
Mokshad <mokshadsankhe@gmail.com>
2026-04-12 02:29:54 -0400

    UI final, Admin Final,

--- backend/routers/init.py
+++ backend/routers/init.py
@@ -4,5 +4,17 @@ from . import hospital
 from . import auth
 from . import sms
 from . import machine
+from . import admin
+from . import appointment
+from . import blood_request
 
-__all__ = ["donor", "hospital", "auth", "sms", "machine"]
+__all__ = [
+    "donor", 
+    "hospital", 
+    "auth", 
+    "sms", 
+    "machine", 
+    "admin", 
+    "appointment", 
+    "blood_request",
+]
